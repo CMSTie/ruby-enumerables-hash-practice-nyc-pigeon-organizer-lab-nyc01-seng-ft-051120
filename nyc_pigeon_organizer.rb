4c817768ec0f87d9bed6cb7d971bf91cf550a4a4
Chris Hosler <cmstie@gmail.com>
2020-04-22 16:59:09 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,7 @@
 def nyc_pigeon_organizer(data)
   name_hash = {}
-  data.each do |key, value|
-    key.each do |key, name|
-      name.each do |value|
+  data.each do |key1, value1|
+    key.each do |key2, names|
+      names.each do |value|
+        if 
 end
